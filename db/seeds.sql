04ff04526375008a513d06e4f805f58f779832a6
Tod Winchester <tod.winchester2@gmail.com>
2023-08-02 12:21:08 -0700

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,42 @@
 
 -- Run INSERT INTO statements for all the three data tables. In other words, you want to seeds all tables with some 
 -- initial records or rows
+const sequelize = require('../config/connection');
+
+const seedViewAllDepartments = require('./department-seed');
+const seedViewAllRoles = require('./roles-seed');
+const seedViewAllEmployees = require('./employees-seed');
+const seedAddADeprtment = require('./department-seed')
+const seedAddARole = require('./role-seed')
+const seedAddAnEmployee = require('./employee-seed')
+const seedUpdateAnEmployeeRole = require('./employee-role-seed')
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n-- DATABASE SYNCED --\n');
+
+    await seedViewAllDepartments();
+    console.log('\n-- DEPARTMENTS SEEDED --\n');
+
+    await seedViewAllRoles();
+    console.log('\n-- ROLES SEEDED --\n');
+    
+    await seedViewAllEmployees();
+    console.log('\n-- EMPLOYEES SEEDED --\n');
+
+    await seedAddADeprtment();
+    console.log('\n-- DEPARTMENT SEEDED --\n');
+
+      await seedAddARole();
+    console.log('\n-- ROLE SEEDED --\n');
+
+      await seedAddAnEmployee();
+    console.log('\n-- EMPLOYEE SEEDED --\n');
+
+      await seedUpdateAnEmployeeRole();
+    console.log('\n-- EMPLOYEE ROLE SEEDED --\n');
+
+    process.exit(0);
+};
+
+seedAll();
